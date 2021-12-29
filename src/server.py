6efa2cefcace88081f36709f04b8fc211169b1f5
mihirworkspace <mihirdeo16@gmail.com>
2021-12-29 23:06:45 +0530

    Fix the  os path issue

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ if __name__=='__main__':
 
     current_path = os.getcwd()
     temp_path = os.path.join(current_path,'temp')
-    if os._exists(temp_path)==False:
+    if os.path.exists(temp_path)==False:
         os.mkdir('temp')
     
     # Launch the API server
